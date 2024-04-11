436e7681570a3eb81f21da29aa690bf68238b76c
Daniel M <danielmino926@gmail.com>
2024-04-11 20:15:19 -0300

    add rel canonical

--- src/components/common/NavbarComponent.tsx
+++ src/components/common/NavbarComponent.tsx
@@ -2,9 +2,11 @@
 import { CUSTOMPATHS } from "@/util/enums";
 import { Dropdown } from "flowbite-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const NavbarComponent = () => {
+  const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
 
   const [activeList, setActiveList] = useState<string | null>(null);
@@ -12,6 +14,8 @@ const NavbarComponent = () => {
   const toggleList = (listId: string) => {
     setActiveList(activeList === listId ? null : listId);
   };
+
+
 
   return (
     <nav className="w-full z-[999] px-10 py-5 headerScroll fixed">
@@ -21,72 +25,75 @@ const NavbarComponent = () => {
         </div>
         <div className="flex justify-end">
           <div className={`md:flex  ${isOpen ? "flex" : "hidden"} gap-5`}>
-            <Link href={CUSTOMPATHS.HOME} className=" hover:text-light text-white">
+            <Link href={CUSTOMPATHS.HOME} rel="Canonical" className=" hover:text-light text-white">
               Home
             </Link>
             <Link href={CUSTOMPATHS.NEWS} className=" hover:text-light text-white">
               Noticias
             </Link>
-            <div className="relative "> 
-              <Link
-                href={CUSTOMPATHS.PRODUCTS}
-                className="peer hover:text-light text-white pb-2 flex items-center"
-                onClick={() => toggleList('list1')}
-              >
-                Productos
-                <svg
-                  className="w-2.5 h-2.5 ms-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 1 4 4 4-4"
-                  />
-                </svg>
-              </Link>
-              <div className={`focus:flex hidden  absolute animate-flip-down  animate-ease-linear animate-duration-200 animate-alternate animate-fill-forwards peer-hover:flex hover:flex flex-col  min-w-[200px] bg-light`}>
-                <Link
-                  className="text-start   px-4 py-3 text-white border-b-1 border-white hover:bg-white hover:text-light"
-                  href={''}
-                >
-                  {" "}
-                  <span className=" hover:translate-x-3  "> Semillas </span>
-                </Link>
-                <Link
-                  className="text-start   px-4 py-3 text-white border-b-1 border-white hover:bg-white hover:text-light"
-                  href={""}
-                >
-                  {" "}
-                  <span className=" hover:translate-x-3  ">
-                    {" "}
-                    Proteccion de cultivo{" "}
-                  </span>
-                </Link>
-                <Link
-                  className="text-start   px-4 py-3 text-white border-b-1 border-white hover:bg-white hover:text-light"
-                  href={""}
-                >
-                  {" "}
-                  <span className=" hover:translate-x-3  ">
-                    {" "}
-                    Fertilizantes{" "}
-                  </span>
-                </Link>
-                <Link
-                  className="text-start   px-4 py-3 text-white  hover:bg-white hover:text-light"
-                  href={""}
-                >
-                  {" "}
-                  <span className=" hover:translate-x-3  ">Varios </span>
-                </Link>
-              </div>
-            </div>
+            {(pathname != CUSTOMPATHS.PRODUCTS)&& (
+ <div className="relative "> 
+ <Link
+   href={CUSTOMPATHS.PRODUCTS}
+   className="peer hover:text-light text-white pb-2 flex items-center"
+   onClick={() => toggleList('list1')}
+ >
+   Productos
+   <svg
+     className="w-2.5 h-2.5 ms-3"
+     aria-hidden="true"
+     xmlns="http://www.w3.org/2000/svg"
+     fill="none"
+     viewBox="0 0 10 6"
+   >
+     <path
+       stroke="currentColor"
+       strokeLinecap="round"
+       strokeLinejoin="round"
+       strokeWidth="2"
+       d="m1 1 4 4 4-4"
+     />
+   </svg>
+ </Link>
+ <div className={`focus:flex hidden  absolute animate-flip-down  animate-ease-linear animate-duration-200 animate-alternate animate-fill-forwards peer-hover:flex hover:flex flex-col  min-w-[200px] bg-light`}>
+   <Link
+     className="text-start   px-4 py-3 text-white border-b-1 border-white hover:bg-white hover:text-light"
+     href={''}
+   >
+    
+     <span className=" hover:translate-x-3  "> Semillas </span>
+   </Link>
+   <Link
+     className="text-start   px-4 py-3 text-white border-b-1 border-white hover:bg-white hover:text-light"
+     href={""}
+   >
+   
+     <span className=" hover:translate-x-3  ">
+  
+       Proteccion de cultivo{" "}
+     </span>
+   </Link>
+   <Link
+     className="text-start   px-4 py-3 text-white border-b-1 border-white hover:bg-white hover:text-light"
+     href={""}
+   >
+  
+     <span className=" hover:translate-x-3  ">
+       
+       Fertilizantes{" "}
+     </span>
+   </Link>
+   <Link
+     className="text-start   px-4 py-3 text-white  hover:bg-white hover:text-light"
+     href={""}
+   >
+     
+     <span className=" hover:translate-x-3  ">Varios </span>
+   </Link>
+ </div>
+</div>
+            )}
+           
             <div className="relative ">
               <Link
                 href={""}
