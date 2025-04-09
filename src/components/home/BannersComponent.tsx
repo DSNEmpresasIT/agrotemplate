5c5f7471f1cf89b559637ae845ff9a559a3591c0
Federico <federicoburgos.dev@gmail.com>
2025-04-09 19:35:50 -0300

    feat: add loading condition to banners component

--- src/components/home/BannersComponent.tsx
+++ src/components/home/BannersComponent.tsx
@@ -38,27 +38,33 @@ const BannersComponent = ({}) => {
   return (
     <>
       <div className="flex h-[300px] sm:h-[426px] bg-gray-400">
-        { banners && banners.length > 0 ?
-          <Swiper autoplay={true} modules={[Controller, Navigation, Autoplay]} className='h-full w-full'>
-            { banners.map((banner: any, index: number) => (
-                <SwiperSlide key={index}>
-                  <Link className='relative flex flex-col justify-center items-center h-full bg-cover bg-center bg-no-repeat'
-                  style={{ backgroundImage: `url(${banner.images.length > 0 ? banner.images[0].url : ''})` }} href={banner.link || CUSTOMPATHS.HOME}>
-                    {
-                      banner.title &&
-                      <h2 className='text-2xl md:text-4xl pb-2 lg:text-5xl md:max-w-[700px] lg:max-w-[900px] w-full text-center text-white'>{banner.title}</h2>
-                    }
-                    {
-                      banner.description &&
-                      <p className='text-md md:text-2xl md:max-w-[700px] lg:max-w-[900px] w-full text-center text-white '>{banner.description}</p>
-                    }
-                  </Link>
-                </SwiperSlide>
-              ))
+        {
+          !loading && 
+          <div className='h-full w-full'>
+            {banners && banners.length > 0 ?
+              <Swiper autoplay={true} modules={[Controller, Navigation, Autoplay]} className='h-full w-full'>
+                {banners.map((banner: any, index: number) => (
+                  <SwiperSlide key={index}>
+                    <Link className='relative flex flex-col justify-center items-center h-full bg-cover bg-center bg-no-repeat'
+                      style={{ backgroundImage: `url(${banner.images.length > 0 ? banner.images[0].url : ''})` }} href={banner.link || CUSTOMPATHS.HOME}>
+                      {
+                        banner.title &&
+                        <h2 className='text-2xl md:text-4xl pb-2 lg:text-5xl md:max-w-[700px] lg:max-w-[900px] w-full text-center text-white'>{banner.title}</h2>
+                      }
+                      {
+                        banner.description &&
+                        <p className='text-md md:text-2xl md:max-w-[700px] lg:max-w-[900px] w-full text-center text-white '>{banner.description}</p>
+                      }
+                    </Link>
+                  </SwiperSlide>
+                ))
+                }
+              </Swiper> :
+              <CarrouselComponent data={bannerData} />
             }
-          </Swiper> :
-          <CarrouselComponent data={bannerData} />
+          </div>
         }
+        
       </div>
     </>
   )
