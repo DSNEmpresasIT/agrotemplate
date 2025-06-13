9a62f1e76ec0418046a47fef8476ebd460f23530
fedeburgos <federicoburgos.dev@gmail.com>
2025-06-13 10:37:22 -0300

    refactor: improve template structure in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,13 @@ import { ProductFetchType } from '@/util/types/types'
 import { InstitutionalSectionComponent } from '@/components/home/InstitutionalSectionComponent'
 import BannersComponent from '@/components/home/BannersComponent'
 import { HeroSection } from '@/components/home/HeroSection'
+import { GiPlantSeed } from "react-icons/gi";
+import { FaSeedling } from "react-icons/fa6";
+import { FaBugSlash } from "react-icons/fa6";
+import { GiFruitTree } from "react-icons/gi";
+import { PlagueBanner } from '@/components/home/PlagueBanner'
+
+
 
 const addProductJsonLd = (): { __html: string } => {
   return {
@@ -60,6 +67,10 @@ export default  function Home() {
     productType: ProductFetchType.OFFERS
   };
 
+  const verticalFadeGradient: React.CSSProperties = {
+    background: 'linear-gradient(180deg, rgba(217, 217, 217, 0.50) 8.65%, rgba(217, 217, 217, 0.40) 31.73%, rgba(217, 217, 217, 0.30) 49.52%, rgba(217, 217, 217, 0.20) 71.15%, rgba(217, 217, 217, 0.00) 91.35%)'
+  };
+
   return (
     <>
        <Head>
@@ -74,27 +85,63 @@ export default  function Home() {
       <div className='min-h-screen bg-[#EFEFEF] w-full flex flex-col'>
       <BannersComponent />
       <div className='flex flex-col mb-20 mx-auto pt-11 md:pt-[87px] w-full justify-center'>
-        <HeroSection/>
 
-        <CategorySection />      
+        <HeroSection/>
+        <div className='max-w-wrapper px-4 mx-auto w-full'>
+          <CategorySection />
+        </div>
         {/* <CategoryExplorerComponent /> */}
 
-        <InstitutionalSectionComponent/>
-        <section className='relative'>
-          <img src="assets/images/plants.png" alt="" className='select-none object-cover h-[287px]'/>
-          <div className='absolute flex flex-col gap-4 justify-center items-center text-white inset-0'>
-            <div className='rounded-[50px] bg-[#8AAE2D] py-5 px-20 text-center'>
-              <h2 className='text-size-title font-bold leading-none'>NO MÁS PLAGAS</h2>
-              <span className='text-size-subtle leading-none'>SABEMOS LO QUE TU TIERRA NECESITA</span>
-            </div>
-            <div className='rounded-[50px] bg-[#36450C] font-medium text-size-subtle py-5 px-10'>
-              <h3 className='leading-none text-center'>Chequeá todos nuestros insecticidas</h3>
+        <div className='relative w-full'>
+          <div className='max-w-wrapper mx-auto px-4'>
+            <div style={verticalFadeGradient} className='rounded-t-[30px]'>
+              <InstitutionalSectionComponent />
+              <PlagueBanner/>
+
+              <section className='flex gap-y-10 gap-x-20 mx-auto px-4 text-[#3F5605] justify-center flex-wrap my-[100px]'>
+                <div className='flex flex-col text-center max-w-[240px] w-full'>
+                  <GiPlantSeed className='mx-auto text-[90px]' />
+                  <div className='flex flex-col'>
+                    <h3 className='text-size-item font-bold my-2'>Lorem, ipsum.</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod dignissimos maxime quidem necessitatibus illo nulla, dolores assumenda quas corporis?</p>
+                  </div>
+                </div>
+                <div className='flex flex-col text-center max-w-[240px] w-full'>
+                  <FaSeedling className='mx-auto text-[90px]' />
+                  <div className='flex flex-col'>
+                    <h3 className='text-size-item font-bold my-2'>Lorem, ipsum.</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod dignissimos maxime quidem necessitatibus illo nulla, dolores assumenda quas corporis?</p>
+                  </div>
+                </div>
+                <div className='flex flex-col text-center max-w-[240px] w-full'>
+                  <FaBugSlash className='mx-auto text-[90px]' />
+                  <div className='flex flex-col'>
+                    <h3 className='text-size-item font-bold my-2'>Lorem, ipsum.</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod dignissimos maxime quidem necessitatibus illo nulla, dolores assumenda quas corporis?</p>
+                  </div>
+                </div>
+                <div className='flex flex-col text-center max-w-[240px] w-full'>
+                  <GiFruitTree className='mx-auto text-[90px]' />
+                  <div className='flex flex-col'>
+                    <h3 className='text-size-item font-bold my-2'>Lorem, ipsum.</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod dignissimos maxime quidem necessitatibus illo nulla, dolores assumenda quas corporis?</p>
+                  </div>
+                </div>
+              </section>
+              <CategorySection />
             </div>
           </div>
-        </section>
-        <ProductSlider {...productSliderData} />
+        </div>
+
+        
+
+        
+
+        
+
+        {/* <ProductSlider {...productSliderData} />
         <ProductSlider {...productSliderDataOffers} />
-        <BestSellers/>
+        <BestSellers/> */}
         {/* <AboutComponent/> */}
         <SponsorsComponent/>
       </div> 
