dc9838c83652d86aa2fd6d1510223bc993e8d79e
fedeburgos <federicoburgos.dev@gmail.com>
2025-06-13 10:35:58 -0300

    style: category section responsive

--- src/components/home/CategorySection.tsx
+++ src/components/home/CategorySection.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { Autoplay, Controller, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FaChevronLeft } from "react-icons/fa6";
+import Link from 'next/link';
+import { CUSTOMPATHS } from '@/util/enums';
 
 const CategorySection: React.FC = () => {
 
@@ -24,13 +26,13 @@ const CategorySection: React.FC = () => {
   return (
     <section className='px-4 max-w-wrapper w-full mx-auto text-[#3F5605] mb-[90px] pb-10'>
       <div>
-        <div className="w-full flex items-center gap-2 mb-3 flex-col">
-          <h2 className="text-size-subtle font-semibold text-center">Categorías principales</h2>
+        <div className="w-full flex flex-col">
+          <h2 className="text-size-subtle leading-none font-medium mx-auto w-full carousel-custom-wrapper">Destacados</h2>
         </div>
         <div className='relative'>
           {showNavigation && (
-            <button aria-label='Anterior' title='Anterior' type='button' className="w-14 aspect-square p-1 swiper-button-prev-related absolute left-0 top-1/2 transform -translate-y-1/2 rounded-full cursor-pointer z-10 flex justify-center items-center bg-[#F8F8F8] shadow-md">
-              <FaChevronLeft className='text-size-item'/>
+            <button aria-label='Anterior' title='Anterior' type='button' className="w-6 md:w-8 lg:w-14 aspect-square p-1 swiper-button-prev-related absolute left-0 top-1/2 transform -translate-y-1/2 rounded-full cursor-pointer z-10 flex justify-center items-center bg-[#F8F8F8] shadow-md">
+              <FaChevronLeft className='text-size-paragraph lg:text-size-item'/>
             </button>
           )}
           <Swiper
@@ -38,13 +40,21 @@ const CategorySection: React.FC = () => {
             grabCursor={true}
             breakpoints={{
               0: {
-                slidesPerView: 3,
+                slidesPerView: 2,
                 spaceBetween: 10,
               },
-              900: {
+              640: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+              },
+              1024: {
                 slidesPerView: 4,
                 spaceBetween: 20,
               },
+              1300: {
+                slidesPerView: 5,
+                spaceBetween: 20,
+              }
             }}
             coverflowEffect={{ rotate: 0, stretch: 0, modifier: 1, slideShadows: true }}
             watchOverflow={false}
@@ -55,21 +65,22 @@ const CategorySection: React.FC = () => {
             loop={false}
             // autoplay={simplifiedCategories.length > slidesToShow}
             modules={[Controller, Navigation, Autoplay]}
-            className="h-full flex mx-auto w-full"
-            style={{ maxWidth: 'calc(100% - 192px)' }}
+            className="h-full flex mx-auto w-full carousel-custom-wrapper"
           >
             {simplifiedCategories?.map((item: any, i: number) => (
               <SwiperSlide title={item.title} key={i} className="bg-white my-5 shadow-md overflow-hidden relative rounded-2xl w-[82.71px] md:w-[342px] md:max-w-none">
-                <img src={item.image && item.image.url || 'assets/images/placeholder.png'} alt="" className='w-full aspect-video bg-[#FAF9F9] object-contain' />
-                <div className='p-8 pt-3 bg-[#FAF9F9]'>
-                  <span className='text-size-item text-center line-clamp-1'>{item.title}</span>
-                </div>
+                <Link href={`${CUSTOMPATHS.CATALOG}/${item.slug}`}>
+                  <img src={item.image && item.image.url || 'assets/images/placeholder.png'} alt="" className='w-full aspect-square bg-[#FAF9F9] object-contain' />
+                  <div className='p-2 md:p-3 lg:pt-4 md:pb-4 xl:pb-8 bg-[#FAF9F9]'>
+                    <span className='text-size-paragraph text-center line-clamp-1'>{item.title}</span>
+                  </div>
+                </Link>
               </SwiperSlide>
             ))}
           </Swiper>
           {showNavigation && (
-            <button aria-label='Siguiente' title='Siguiente' type='button' className="w-14 aspect-square p-1 swiper-button-next-related absolute right-0 top-1/2 transform -translate-y-1/2 rounded-full cursor-pointer z-10 flex justify-center items-center bg-[#F8F8F8] shadow-md">
-              <FaChevronLeft className='text-size-item rotate-180'/>
+            <button aria-label='Siguiente' title='Siguiente' type='button' className="w-6 md:w-8 lg:w-14 aspect-square p-1 swiper-button-next-related absolute right-0 top-1/2 transform -translate-y-1/2 rounded-full cursor-pointer z-10 flex justify-center items-center bg-[#F8F8F8] shadow-md">
+              <FaChevronLeft className='text-size-paragraph lg:text-size-item rotate-180'/>
             </button>
           )}
         </div>
