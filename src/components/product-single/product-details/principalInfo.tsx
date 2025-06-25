cd24f1d47267d41e4c1625851af9543d089d7f7c
fedeburgos <federicoburgos.dev@gmail.com>
2025-06-25 16:19:27 -0300

    style: add new style to product info table

--- src/components/product-single/product-details/principalInfo.tsx
+++ src/components/product-single/product-details/principalInfo.tsx
@@ -7,13 +7,13 @@ interface Props {
 
 const PrincipalInfo = ({ principalInfo }: Props) => {
   return (
-    <ul className="flex flex-col">
+    <ul className="flex flex-col max-w-[900px] w-full mt-7">
       {principalInfo?.items && principalInfo.items.length > 0 ? (
         principalInfo.items.map((item, index) => (
-          <li key={index} className="py-4 border-b-2">
-            <div className="flex flex-col">
-              <h6 className='text-black font-semibold'>{item.title}</h6>
-              <p className='text-slate-600'>{item.text}</p>
+          <li key={index} className="border-b-2 border-gray-300 group w-full last:border-none">
+            <div className="flex flex-col sm:flex-row sm:gap-5 sm:items-center w-full text-size-paragraph">
+              <h6 className='text-black font-medium min-w-[200px] p-3 sm:p-0'>{item.title}</h6>
+              <p className='text-slate-600 bg-[#d9d9d9]/40 sm:group-first:rounded-t-[20px] sm:group-last:rounded-b-[20px] p-3 w-full'>{item.text}</p>
             </div>
           </li>
         ))
