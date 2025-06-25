354526e74de7f25df28a57e444a3caac6ae9d08b
fedeburgos <federicoburgos.dev@gmail.com>
2025-06-25 16:33:49 -0300

    style: add font-size classes to banners component

--- src/components/home/BannersComponent.tsx
+++ src/components/home/BannersComponent.tsx
@@ -48,14 +48,14 @@ const BannersComponent = ({}) => {
                       style={{ backgroundImage: `url(${banner.images.length > 0 ? banner.images[0].url : ''})`, alignItems: banner.contentPosition }} href={banner.link || CUSTOMPATHS.HOME}>
                       {banner.title &&
                         <>
-                          <h2 className='md:hidden text-center text-xl md:text-4xl pb-2 lg:text-5xl md:max-w-[700px] lg:max-w-[900px] w-full text-white uppercase'>{banner.title}</h2>
-                          <h2 style={{textAlign: banner.contentPosition}} className='hidden md:block text-2xl md:text-4xl pb-2 lg:text-5xl md:max-w-[700px] lg:max-w-[900px] w-full text-white uppercase'>{banner.title}</h2>
+                          <h2 className='md:hidden text-center pb-2 md:max-w-[700px] lg:max-w-[900px] w-full text-white uppercase text-size-title'>{banner.title}</h2>
+                          <h2 style={{textAlign: banner.contentPosition}} className='hidden md:block pb-2 md:max-w-[700px] lg:max-w-[900px] w-full text-white uppercase text-size-title'>{banner.title}</h2>
                         </>
                       }
                       {banner.description &&
                         <>
-                          <p className='hidden sm:block md:hidden text-center text-md md:text-2xl md:max-w-[700px] lg:max-w-[900px] w-full text-white '>{banner.description}</p>
-                          <p style={{textAlign: banner.contentPosition}} className='hidden md:block text-md md:text-2xl md:max-w-[700px] lg:max-w-[900px] w-full text-white'>{banner.description}</p>
+                          <p className='hidden sm:block md:hidden text-size-item text-center md:max-w-[700px] lg:max-w-[900px] w-full text-white '>{banner.description}</p>
+                          <p style={{textAlign: banner.contentPosition}} className='hidden md:block md:max-w-[700px] lg:max-w-[900px] w-full text-size-item text-white'>{banner.description}</p>
                         </>
                       }
                     </Link>
