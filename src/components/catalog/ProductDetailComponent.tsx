5c0b4f3279f1980c283c1ec1665b5ace06c86ed1
fedeburgos <federicoburgos.dev@gmail.com>
2025-07-02 15:16:51 -0300

    feat: implement images and attributes components in product detail page

--- src/components/catalog/ProductDetailComponent.tsx
+++ src/components/catalog/ProductDetailComponent.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useCart } from "@/context/cart-context/cart-context";
-import { Product } from "@/util/types/types";
+import { Images, Product } from "@/util/types/types";
 import Link from "next/link";
 import NavDetails from "../product-single/product-details/navDetails";
 import { RelatedProductsSection } from "../product-single/RelatedProductsSection";
@@ -14,12 +14,32 @@ import toast from "react-hot-toast";
 import DOMPurify from "dompurify";
 import SliderProductImg from "./SliderProductImg";
 import SafeHTMLComponent from "../common/SafeHTMLComponent";
+import { useEffect, useState } from "react";
+import { getAttributes } from "@/services/api/attributes-service";
+import PreviewCarousel from "../common/PreviewCarousel";
+import ProductDetailImages from "./ProductDetailImages";
+import ProductDetailAttributes from "./ProductDetailAttributes";
 
 interface ProductDetailProps {
   product: Product;
 }
 
 export const ProductDetailComponent = ({ product }: ProductDetailProps) => {
+
+  const [productAttributes, setProductAttributes] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchAttributes = async () => {
+      if (!product?.id) return;
+      try {
+        const attributes = await getAttributes(product.id);
+        setProductAttributes(attributes[0]);
+      } catch (error) {
+        console.error('Error obteniendo atributos del producto:', error);
+      }
+    };
+    fetchAttributes();
+  }, [product]);
 
   const dispatch = useDispatch();
   const handleAddToCart = () => {
@@ -32,7 +52,7 @@ export const ProductDetailComponent = ({ product }: ProductDetailProps) => {
         Producto agregado al presupuesto
         <button
           onClick={() => dispatch(toggleCartVisibility())}
-          className="bg-white text- px-2 py-1 rounded text-sm hover:underline"
+          className="bg-white px-2 py-1 rounded text-size-paragraph hover:underline"
         >
           Ver presupuesto
         </button>
@@ -49,46 +69,31 @@ export const ProductDetailComponent = ({ product }: ProductDetailProps) => {
 
   return (
     <>
-      <Banner title={`Producto ${sanitizeProductName(product?.name)}`}></Banner>
-      <div className="flex flex-col flex-grow w-full mx-auto max-w-wrapper gap-20 mt-10 mb-20 px-4">
-        <section className="flex w-full flex-col justify-center items-center gap-20">
-          <div className="grid grid-cols-1 sm:grid-cols-2 w-full gap-10 ">
-            <div className="w-full">
-              {
-                product?.images && (
-                  <SliderProductImg images={product.images}/>
-                )
-              }
-
+      <Banner title={`${sanitizeProductName(product?.name)}`}></Banner>
+      <div className="w-full mx-auto px-4 flex flex-col flex-grow">
+        <div className="max-w-main-wrapper mx-auto w-full flex flex-grow px-4 mt-10">
+          <div className="w-full mx-auto grid md:grid-cols-2 gap-10 main-wrapper-gradient p-10 rounded-t-[30px]">
+            <div className="flex flex-col gap-10">
+              <ProductDetailImages images={product.images!} />
+              <ProductDetailAttributes productAttributes={productAttributes} />
             </div>
-
-            <div className="flex flex-col w-full flex-grow justify-between">
-              <div>
-                <h1 className="text-black font-semibold text-size-subtle capitalize">{product?.name}</h1>
+            <div className="relative pb-10">
+              <div className="sticky top-[113px] flex flex-col gap-3 items-start">
+                <div className="">nav links / nav links</div>
                 {
-                  product.description && 
-                  <div className="mt-4">
-                    <h3 className="text-black font-semibold pb-2  text-lg">Descripción del producto</h3>
-                    <SafeHTMLComponent html={product.description}/>
-                  </div>
+                  product.description && (
+                    <>
+                      <h2 className="text-size-subtle font-medium text-cc-green">{product?.name}</h2>
+                      <SafeHTMLComponent html={product.description} />
+                    </>
+                  )
                 }
-              </div>
-
-              <div className=" flex flex-col gap-6">
-                <p className='border-b w-full border-slate-500/50 text-slate-500  border-bottom d-inline-flex w-100 border-secondary'>Conseguí el mejor precio</p>
-                <div className="flex flex-col lg:flex-row gap-4">
-                  <Link className='bg-light lab-btn font-semibold w-full px-4 py-4' href={`https://api.whatsapp.com/send?phone=5493454037365&text=Hola, me gustaría saber mas información sobre el producto ${product?.name}`}><span className="text-center">Consultar</span></Link>
-                  <button onClick={handleAddToCart} className="bg-light justify-center lab-btn font-semibold w-full flex  px-2 py-4"><span className="flex  justify-center text-center">Agregar al presupuesto<TbShoppingCartQuestion className="text-2xl " /></span></button>
-                </div>
+                <button onClick={handleAddToCart} className="px-10 mt-10 py-2 rounded-lg bg-cc-green text-white"><span className="flex justify-center text-center">Agregar al presupuesto<TbShoppingCartQuestion className="text-2xl ms-3" /></span></button>
               </div>
             </div>
           </div>
-        </section>
-
-        <NavDetails data={product?.product_features}  ></NavDetails>
-        {/* <RelatedProductsSection productSelected={product} /> */}
+        </div>
       </div>
     </>
-
   )
 }
