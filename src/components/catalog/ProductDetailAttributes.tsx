24f0277dd1306f77743715b697a76feff51ffa7a
fedeburgos <federicoburgos.dev@gmail.com>
2025-07-11 18:04:20 -0300

    style: add margin to category item on product attributes

--- src/components/catalog/ProductDetailAttributes.tsx
+++ src/components/catalog/ProductDetailAttributes.tsx
@@ -8,7 +8,7 @@ const ProductDetailAttributes: React.FC<Props> = ({ productAttributes }) => {
     <div className="rounded-xl bg-[#DEDEDE66] p-4 md:p-10 flex flex-col gap-3">
       { 
         productAttributes.map((category: any, index: number) => (
-          <div key={index} className="flex flex-col">
+          <div key={index} className="flex flex-col mt-4 first:mt-0">
             <h3 className="text-size-item text-cc-green font-medium">{category.categoryName}</h3>
             <div className="flex flex-col">
               {
