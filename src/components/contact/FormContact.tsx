df6b49d054c1a4ca07bbbd4156e69845846a1220
fedeburgos <federicoburgos.dev@gmail.com>
2025-07-12 13:31:07 -0300

    style: add spacing to contact form

--- src/components/contact/FormContact.tsx
+++ src/components/contact/FormContact.tsx
@@ -74,14 +74,14 @@ const FormContact:FC<FormContactProps> = ({ keys }) => {
 
   return (
     <div>
-      <form className="flex flex-col gap-3 max-w-[500px] text-size-paragraph text-cc-very-dark-green" onSubmit={handleSubmit}>
+      <form className="flex flex-col gap-5 max-w-[500px] text-size-paragraph text-cc-very-dark-green" onSubmit={handleSubmit}>
         <h3 className='text-cc-green text-size-item'>Rellena tus datos y dejanos saber todas tus dudas y sugerencias.</h3>
-        <div className='flex lg:flex-row flex-col gap-2'>
+        <div className='flex lg:flex-row flex-col gap-5'>
           <input onChange={handleChangeData} className='bg-[#59731414] w-full border text-size-paragraph rounded-md border-none focus:ring-cc-light-green' type="text" name="user_name" value={data.user_name} placeholder="Nombre o empresa*" />
           <input onChange={handleChangeData} className='bg-[#59731414] w-full border text-size-paragraph rounded-md border-none focus:ring-cc-light-green' type="email" name="user_email" value={data.user_email} placeholder="Mail*" />
         </div>
         <input onChange={handleChangeData} className='bg-[#59731414] border text-size-paragraph rounded-md border-none focus:ring-cc-light-green' type="text" name="user_phone" value={data.user_phone} placeholder="Telefono*" />
-        <textarea onChange={handleChangeData} className='bg-[#59731414] border text-size-paragraph rounded-md border-none focus:ring-cc-light-green' name="message" id="role" cols={30} rows={10} value={data.message} placeholder="Mensaje*"></textarea>
+        <textarea onChange={handleChangeData} className='bg-[#59731414] h-28 py-2 border text-size-paragraph rounded-md border-none focus:ring-cc-light-green' name="message" id="role" cols={30} rows={10} value={data.message} placeholder="Mensaje*"></textarea>
         {
           showCaptcha
           // @ts-ignore: Unreachable code error
